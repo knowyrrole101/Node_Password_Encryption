5c7c650df05f72bdbc10eca31600cec0d62bcef3
Moon Ahmed <planetbarsent@gmail.com>
2016-01-27 19:32:14 -0800

    Data encryption and de-cryption methods

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 console.log("Starting Password Manager");
-
 var crypto = require('crypto-js');
 var storage = require('node-persist');
+//<------------------------->//
 var argv = require('yargs')
   //Create Account Command
   .command('create', 'Create new account', function(yargs){
@@ -47,38 +47,40 @@ var argv = require('yargs')
         description: 'Master Password',
         string: 'string'
       }
-    }).help('help')
+    }).help('help');
   })
   .help('help')
   .argv;
-//array that stores value in _[0]
+//<---------------------------->//
 //Argument Command store
 var command = argv._[0];
-//Argument Data
+//Argv data
 var data = argv;
 //Initialize Storage Sync
 //from node persist
 storage.initSync();
+//<------------------------------>//
 
 function get_accounts(master_password){
   //Pull from Persist
   var encrypted_accounts = storage.getItemSync('accounts');
-  var accounts = [];
+  var accounts=[]; 
   //Decrypt using master_password and passing in the encrypted_accounts
   if(typeof encrypted_accounts !== 'undefined'){
     var bytes = crypto.AES.decrypt(encrypted_accounts, master_password);
     //JSON Accounts
     accounts = JSON.parse(bytes.toString(crypto.enc.Utf8));
-  };
+  }
+  
   return accounts;
 }
 
 function save_accounts(accounts,master_password){
-  //
-  var encrypted_accounts = crypto.AES.encrypt(JSON.stringify(accounts),master_password);
+  //Encrypt JSON Data
+  var encrypted_accounts = crypto.AES.encrypt(JSON.stringify(accounts), master_password);
   //Store in persist the encrypted data in accounts
-  storage.setItemSync('accounts', encrypted_accounts.toString(crypto.enc.Utf8));
-  //return accounts array
+  storage.setItemSync('accounts', encrypted_accounts.toString());
+  
   return accounts;
 }
 
@@ -87,19 +89,22 @@ function create_account(account, master_password){
   //push object created by create command into accounts hash
   accounts.push(account);
   save_accounts(accounts, master_password);
-  //return account;
-};
+  
+  return account;
+}
 
 function get_account(account_name,master_password){
   var accounts = get_accounts(master_password);
   var matched_account;
+  //Loop through JSON object to find account.account_name
   accounts.forEach(function(account){
     if(account.account_name === account_name){
       matched_account = account;
     }
   });
+  
   return matched_account;
-};
+}
 
 var execute_account_command = function(){
   if(command==='create'){
@@ -110,9 +115,10 @@ var execute_account_command = function(){
     },data.master_password);
     console.log("Account Created");
     console.log(created_account);
-  } else if(command==='get'){
+  } 
+  else if(command==='get'){
     var stored_account = get_account(data.account_name,data.master_password);
-    console.log("Retrieving Account...")
+    console.log("Retrieving Account...");
     console.log(stored_account);
   }
 };
